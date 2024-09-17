2d634c9b307e9bdff5d12dd169d4478ea26113fd
unknown <abhishekmishra992016@gmail.com>
2024-09-17 12:53:16 +0530

    added get user and middleware in apis

--- controllers/authControllers/authControllers.js
+++ controllers/authControllers/authControllers.js
@@ -46,7 +46,6 @@ const createUser = async (req, res) => {
     }
 };
 
-
 const userLogin = async (req, res) => {
     const { email, password, type } = req.body;
 
@@ -137,7 +136,7 @@ const verifyOtp = async (req, res) => {
                 { expiresIn: "1h" }
             );
 
-            res.status(200).json({ message: "OTP verified successfully", token });
+            res.status(200).json({ message: "OTP verified successfully", token, user: user._id });
         } else {
             otpRecord.attempts += 1;
             await otpRecord.save();
@@ -149,9 +148,63 @@ const verifyOtp = async (req, res) => {
     }
 };
 
-
 const forgotPassword = async (req, res) => {
+    const { userId, newPassword } = req.body;
 
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+        user.password = hashedPassword;
+        await user.save();
+        res.status(200).json({ message: "Password reset successful" });
+    } catch (error) {
+        console.error("Error during password reset:", error);
+        res.status(500).json({ message: "Failed to reset password", error: error.message });
+    }
 }
 
-export { createUser, userLogin, verifyOtp };
+const removeUser = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        await User.deleteOne({ _id: id });
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        console.error("Error during user deletion:", error);
+        res.status(500).json({ message: "Failed to delete user", error: error.message });
+    }
+}
+
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Failed to fetch users", error: error.message });
+    }
+}
+
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Failed to fetch users", error: error.message });
+    }
+}
+
+export { createUser, userLogin, verifyOtp, forgotPassword, removeUser, getUserById,getAllUsers };
--- routes/authRoutes/authRoutes.js
+++ routes/authRoutes/authRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-import { createUser, userLogin, verifyOtp } from "../../controllers/authControllers/authControllers.js";
+import authMiddleware from "../../Middlewares/authMiddleware.js";
+import { createUser, userLogin, verifyOtp, forgotPassword, removeUser, getUserById, getAllUsers } from "../../controllers/authControllers/authControllers.js";
 
 router.post('/signup', createUser);
 router.post('/signin', userLogin);
 router.post('/verify', verifyOtp);
-
+router.put('/forgotpassword', forgotPassword);
+router.get('/:id', authMiddleware, getUserById);
+router.get('/', authMiddleware, getAllUsers);
+router.delete('/removeUser/:id', authMiddleware, removeUser);
 
 export default router;
 
