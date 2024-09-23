7ca91fc0fd8678936015ba50e567d5373d31062f
unknown <abhishekmishra992016@gmail.com>
2024-09-23 17:58:15 +0530

    added middle ware

--- routes/authRoutes/authRoutes.js
+++ routes/authRoutes/authRoutes.js
@@ -14,14 +14,14 @@ import {
     createUsersByCsvFile
 } from "../../controllers/authControllers/authControllers.js";
 
-router.post('/signup', createUser);
+router.post('/signup', authMiddleware,createUser);
 router.post('/signin', userLogin);
 router.post('/verify', verifyOtp);
 router.put('/forgotpassword', forgotPassword);
 router.get('/:id', authMiddleware, getUserById);
 router.get('/', authMiddleware, getAllUsers);
 router.delete('/removeUser/:id', authMiddleware, removeUser);
-router.put('/update/:id', updateUserDetails);
+router.put('/update/:id', authMiddleware,updateUserDetails);
 router.post('/createuserbycsv', createUsersByCsvFile);
 
 export default router;
