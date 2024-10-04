78875fb434994eaa1ea434b82248b354052eef38
unknown <abhishekmishra992016@gmail.com>
2024-10-04 17:40:34 +0530

    make changeg in the subject api

--- routes/subjectRoutes/subjectRoute.js
+++ routes/subjectRoutes/subjectRoute.js
@@ -2,11 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import { createSubject, removeSubject, getSubjectById, getAllSubjects, updateSubject } from "../../controllers/classControllers/subjectControllers.js";
+import authMiddleware from "../../Middlewares/authMiddleware.js";
 
-router.post("/create/subject", createSubject);
-router.delete("/remove/subject/:id", removeSubject);
-router.get("/getbyid/subject/:id", getSubjectById);
-router.get("/getall/subject", getAllSubjects);
-router.put("/update/subject/:id", updateSubject);
+router.post("/create/subject",authMiddleware, createSubject);
+router.delete("/remove/subject/:id",authMiddleware, removeSubject);
+router.get("/getbyid/subject/:id",authMiddleware, getSubjectById);
+router.get("/getall/subject",authMiddleware, getAllSubjects);
+router.put("/update/subject/:id",authMiddleware, updateSubject);
 
 export default router;
