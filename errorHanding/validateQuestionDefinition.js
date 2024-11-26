da5aba06b66b9137f4a56f053677115f4fb0a745
unknown <abhishekmishra992016@gmail.com>
2024-11-26 18:11:21 +0530

    made changs in the scheme creation

--- errorHanding/validateQuestionDefinition.js
+++ errorHanding/validateQuestionDefinition.js
@@ -13,24 +13,24 @@ export const validateQuestionDefinition = ({
         return "Missing required fields: schemaId, questionsName, maxMarks, or minMarks.";
     }
 
-    if (minMarks > maxMarks) {
+    if (Number(minMarks) > Number(maxMarks)) {
         return "Minimum marks must be less than or equal to maximum marks.";
     }
 
     if (!isSubQuestion) {
-        if (!marksDifference || marksDifference <= 0) {
-            return "Marks difference is required and must be greater than 0 when there are no subquestions.";
+        if (!marksDifference || isNaN(Number(marksDifference)) || Number(marksDifference) <= 0) {
+            return "Marks difference is required and must be a positive number when there are no subquestions.";
         }
-        if (bonusMarks <= 0) {
+        if (Number(bonusMarks) <= 0) {
             return "Bonus marks must be greater than 0 when there are no subquestions.";
         }
     } else {
-        if (numberOfSubQuestions <= 0) {
+        if (Number(numberOfSubQuestions) <= 0) {
             return "Number of subquestions must be greater than 0 when there are subquestions.";
         }
         if (
-            compulsorySubQuestions < 0 ||
-            compulsorySubQuestions > numberOfSubQuestions
+            Number(compulsorySubQuestions) < 0 ||
+            Number(compulsorySubQuestions) > Number(numberOfSubQuestions)
         ) {
             return "Compulsory subquestions must be between 0 and the total number of subquestions when there are subquestions.";
         }
