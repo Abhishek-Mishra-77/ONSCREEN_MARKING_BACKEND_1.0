9237556a2c835c4d3db2bbc746516b757cb61c91
unknown <abhishekmishra992016@gmail.com>
2024-12-05 10:58:01 +0530

    made initial user creation module

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
-import bcrypt from "bcryptjs";
 import path from 'path';
 
 import database from "./utils/database.js"
+import createInitialUser from "./services/initialUserCreation.js";
 
-import User from "./models/authModels/User.js"
 import authRoutes from "./routes/authRoutes/authRoutes.js";
 import classRoutes from "./routes/classRoutes/classRoute.js";
 import subjectRoutes from "./routes/subjectRoutes/subjectRoute.js";
@@ -51,45 +50,3 @@ app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-
-
-/* -------------------------------------------------------------------------- */
-/*                           FUNCTION TO CREATE INITIAL USER                  */
-/* -------------------------------------------------------------------------- */
-async function createInitialUser() {
-    try {
-        const existingUser = await User.findOne({ email: "abhishekomr077@gmail.com" });
-        if (!existingUser) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash("12345678", salt);
-            const newUser = new User({
-                name: "Abhishek Mishra",
-                email: "abhishekomr077@gmail.com",
-                password: hashedPassword,
-                mobile: "8577887978",
-                role: "admin",
-                permissions: [
-                    "Dashboard",
-                    "Evaluator Dashboard",
-                    "Classes",
-                    "Courses",
-                    "Course Detail",
-                    "Profile",
-                    "Users",
-                    "Create User",
-                    "Upload CSV File",
-                    "Schema",
-                    "Create Schema",
-                    "Schema Structure"
-                ]
-            });
-
-            await newUser.save();
-            console.log("Initial admin user created");
-        } else {
-            console.log("Admin user already exists");
-        }
-    } catch (error) {
-        console.error("Error creating user:", error);
-    }
-}
